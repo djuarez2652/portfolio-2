3857b57fb1e712eed8ad312ccee43413e1522339
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2024-04-27 23:57:56 -0400

    fixed image rendering issue for safari

--- src/ContactBar.jsx
+++ src/ContactBar.jsx
@@ -35,13 +35,13 @@ export default function ContactBar () {
 				className={'h-[40px] w-[110px] p-1 md:h-[70px] md:w-[160px] flex md:p-4 justify-between relative'}
 			>
 				<a href={'https://github.com/djuarez2652/'} target={'_blank'}>
-					<img src={GithubLogo} className={'h-full'}/>
+					<img src={GithubLogo} className={'h-full w-[32px] md:w-[38px]'} alt={'github'}/>
 				</a>
 				<a onClick={ copyToClipBoard } target={'_blank'}>
-					<img src={GmailLogo} className={'h-full'}/>
+					<img src={GmailLogo} className={'h-full w-[32px] md:w-[38px]'} alt={'gmail'}/>
 				</a>
 				<a href={'https://www.linkedin.com/in/david-juarez-429603261/'} target={'_blank'}>
-					<img src={LinkedInLogo} className={'h-full'}/>
+					<img src={LinkedInLogo} className={'h-full w-[32px] md:w-[38px]'} alt={'linkedin'}/>
 				</a>
 				{
 					showCopy ?
@@ -49,10 +49,10 @@ export default function ContactBar () {
 							className={'absolute rounded-md w-[130%] top-[100%] left-[-20%] p-1 md:w-full md:top-[90%] h-fit md:p-3 md:left-[0%] text-sm text-center bg-slate-600'}
 						>
 							<p>
-								{!copySuccess ? 'Copied Successfully!' : <span>Copy Failed</span>}<br/>
-								{!copySuccess ? '' : <span className={'select-all text-xs'}>dj2652@columbia.edu</span>}
+								{copySuccess ? 'Copied Successfully!' : <span>Copy Failed</span>}<br/>
+								{copySuccess ? '' : <span className={'select-all text-xs'}>dj2652@columbia.edu</span>}
 							</p>
-							<img src={X} className={'h-[15px] w-[15px] md:h-[20px] md:w-[20px] absolute left-[83%] top-[10%] md:left-[85%] md:top-[10%]'} onClick={ handleClose }/>
+							<img src={X} className={'h-[15px] w-[15px] md:h-[20px] md:w-[20px] absolute left-[83%] top-[10%] md:left-[85%] md:top-[10%]'} onClick={ handleClose } alt={'X'}/>
 						</div> :
 						null
 				}
